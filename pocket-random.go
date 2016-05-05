5dcf1268894595f022aab829cc3b2ad30d71877b
Shao-Chung Chen <dannvix@gmail.com>
2016-05-05 22:10:55 +0800

    refactor: pocket-random.go

--- pocket-random.go
+++ pocket-random.go
@@ -13,8 +13,53 @@ import (
 	"os"
 	"os/user"
 	"path"
+	"strconv"
 	"strings"
+	"time"
 )
+
+const (
+	API_BASE = "https://getpocket.com/v3/"
+)
+
+////////////////////////////////////////////////////////////////////////////////
+
+func prettyDateSince(unixTime int) string {
+	then := time.Unix(int64(unixTime), 0)
+	sinceThen := time.Since(then)
+	diffDays := int(sinceThen.Hours() / 24)
+	switch {
+	case diffDays < 0:
+		return "from the future!"
+	case diffDays == 0:
+		diffSeconds := int(sinceThen.Seconds())
+		switch {
+		case diffSeconds < 60:
+			return "few seconds ago"
+		case diffSeconds < 120:
+			return "a minute ago"
+		case diffSeconds < 3600:
+			return fmt.Sprintf("%d minutes ago", int(diffSeconds/60))
+		case diffSeconds < 7200:
+			return "an hour ago"
+		default:
+			return fmt.Sprintf("%d hours ago", int(diffSeconds/3600))
+		}
+	case diffDays == 1:
+		return "yesterday"
+	case diffDays < 7:
+		return fmt.Sprintf("%d days ago", int(diffDays))
+	case diffDays < 31:
+		return fmt.Sprintf("%d weeks ago", int(diffDays/7))
+	case diffDays < 365:
+		return fmt.Sprintf("%d months ago", int(diffDays/30))
+	default:
+		return fmt.Sprintf("%d years ago", int(diffDays/365))
+	}
+	return ""
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 type UserConfig struct {
 	ApiKey    string `json:"api_key"`
@@ -30,7 +75,6 @@ func NewUserConfig() *UserConfig {
 	currentUser, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 
 	cfg := new(UserConfig)
@@ -38,21 +82,19 @@ func NewUserConfig() *UserConfig {
 	return cfg
 }
 
-func (cfg UserConfig) save() {
+func (cfg *UserConfig) save() {
 	cfgJson, err := json.Marshal(cfg)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 
 	err = ioutil.WriteFile(cfg.pathName, cfgJson, 0644)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 }
 
-func (cfg UserConfig) loadOrCreateNew() *UserConfig {
+func (cfg *UserConfig) loadOrInitialize() *UserConfig {
 	raw, err := ioutil.ReadFile(cfg.pathName)
 	if err != nil {
 		cfg.save()
@@ -62,261 +104,197 @@ func (cfg UserConfig) loadOrCreateNew() *UserConfig {
 	if err != nil {
 		cfg.save()
 	}
-	return &cfg
+	return cfg
 }
 
-func main() {
-	const API_BASE = "https://getpocket.com/v3/"
+////////////////////////////////////////////////////////////////////////////////
 
-	// ------------------
-	// read configuration
-	// ------------------
-	cfg := NewUserConfig()
-	cfg = cfg.loadOrCreateNew()
+func requestPocketApi(cfg *UserConfig, requestPath string, requestData url.Values) map[string]interface{} {
+	if cfg.ApiKey != "" {
+		requestData.Add("consumer_key", cfg.ApiKey)
+	}
+	if cfg.UserToken != "" {
+		requestData.Add("access_token", cfg.UserToken)
+	}
 
-	// ---------------
-	// request API key
-	// ---------------
+	requestUrl := API_BASE + requestPath
+	req, err := http.NewRequest("POST", requestUrl, strings.NewReader(requestData.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("X-Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if (err != nil) || (resp.StatusCode != 200) {
+		log.Fatalf("POST '%s' failed, StatusCode=[%d], X-Error-Code=[%s]",
+			requestUrl, resp.StatusCode, resp.Header.Get("X-Error-Code"))
+	}
+	defer resp.Body.Close()
+
+	var bodyJson map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&bodyJson)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bodyJson
+}
+
+func requestApiKey(cfg *UserConfig) {
+	pocketDevSite := "http://getpocket.com/developer/apps/"
+	fmt.Printf("No API key available. Get one on %s\n", pocketDevSite)
+	webbrowser.Open(pocketDevSite)
+
+	fmt.Printf("Enter your API key: ")
+	apiKey, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg.ApiKey = strings.Trim(apiKey, " \t\n")
+	cfg.save()
+}
+
+func requestUserCode(cfg *UserConfig) {
+	const redirectUri = "https://getpocket.com/connected_accounts"
+	requestData := url.Values{
+		"redirect_uri": {redirectUri},
+	}
+
+	body := requestPocketApi(cfg, "oauth/request", requestData)
+	userCode := body["code"].(string)
+	fmt.Printf("OAuth code: %s\n", userCode)
+
+	authorizationUrl := fmt.Sprintf(
+		"https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s",
+		userCode, redirectUri)
+	fmt.Printf("Please authorize this app on %s\n", authorizationUrl)
+	webbrowser.Open(authorizationUrl)
+
+	fmt.Printf("Press any key to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	cfg.UserCode = userCode
+	cfg.save()
+}
+
+func requestUserToken(cfg *UserConfig) {
+	requestData := url.Values{
+		"code": {cfg.UserCode},
+	}
+
+	body := requestPocketApi(cfg, "oauth/authorize", requestData)
+	userName := body["username"].(string)
+	userToken := body["access_token"].(string)
+	fmt.Printf("Username: %s\n", userName)
+	fmt.Printf("Auth token: %s\n", userToken)
+
+	cfg.Username = userName
+	cfg.UserToken = userToken
+	cfg.save()
+}
+
+func requestPermission(cfg *UserConfig) {
 	if cfg.ApiKey == "" {
-		pocketDevSite := "http://getpocket.com/developer/apps/"
-		fmt.Printf("No API key available. Get one on %s\n", pocketDevSite)
-		webbrowser.Open(pocketDevSite)
-
-		fmt.Printf("Enter your API key: ")
-		consoleReader := bufio.NewReader(os.Stdin)
-		apiKey, err := consoleReader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		cfg.ApiKey = strings.Trim(apiKey, " \t\n")
-		cfg.save()
+		requestApiKey(cfg)
 	}
 
-	// -------------------------------------
-	// request OAuth code (open web browser)
-	// -------------------------------------
 	if cfg.UserCode == "" {
-		// FIXME: refactor duplicated HTTP communication code
-		redirectUri := "https://getpocket.com/connected_accounts"
-		requestUrl := fmt.Sprintf("%soauth/request", API_BASE)
-		requestData := url.Values{
-			"consumer_key": {cfg.ApiKey},
-			"redirect_uri": {redirectUri},
-		}
-
-		req, err := http.NewRequest("POST", requestUrl, strings.NewReader(requestData.Encode()))
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("X-Accept", "application/json")
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Request code failed, StatusCode=[%d], X-Error-Code=[%s]",
-				resp.StatusCode, resp.Header.Get("X-Error-Code")))
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		var data map[string]interface{}
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		userCode := data["code"].(string)
-		fmt.Printf("OAuth code: %s\n", userCode)
-
-		authorizationUrl := fmt.Sprintf(
-			"https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s",
-			userCode, redirectUri)
-		fmt.Printf("Please authorize this app on %s\n", authorizationUrl)
-		webbrowser.Open(authorizationUrl)
-
-		fmt.Printf("Press any key to continue...")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-		cfg.UserCode = userCode
-		cfg.save()
+		requestUserCode(cfg)
 	}
 
-	// -------------------
-	// request OAuth token
-	// -------------------
 	if cfg.UserToken == "" {
-		// FIXME: refactor duplicated HTTP communication code
-		requestUrl := fmt.Sprintf("%soauth/authorize", API_BASE)
-		requestData := url.Values{
-			"consumer_key": {cfg.ApiKey},
-			"code":         {cfg.UserCode},
-		}
+		requestUserToken(cfg)
+	}
+}
 
-		req, err := http.NewRequest("POST", requestUrl, strings.NewReader(requestData.Encode()))
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("X-Accept", "application/json")
+func retrieveItems(cfg *UserConfig) []map[string]interface{} {
+	fmt.Printf("Retrieving items from Pocket... ")
 
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Request authorization failed, StatusCode=[%d], X-Error-Code=[%s]",
-				resp.StatusCode, resp.Header.Get("X-Error-Code")))
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		var data map[string]interface{}
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		userName := data["username"].(string)
-		userToken := data["access_token"].(string)
-		fmt.Printf("Username: %s\n", userName)
-		fmt.Printf("Auth token: %s\n", userToken)
-
-		cfg.Username = userName
-		cfg.UserToken = userToken
-		cfg.save()
+	requestData := url.Values{
+		"detailType": {"simple"},
+		"count":      {"10"},
 	}
 
-	// -----------------------------
+	body := requestPocketApi(cfg, "get", requestData)
+	var items []map[string]interface{}
+	for _, item := range body["list"].(map[string]interface{}) {
+		items = append(items, item.(map[string]interface{}))
+	}
+
+	fmt.Printf("%d items retrieved!\n", len(items))
+
+	// shuffle by randomly swap items
+	for i := range items {
+		j := rand.Intn(len(items))
+		items[i], items[j] = items[j], items[i]
+	}
+	return items
+}
+
+func userInteractOnItem(cfg *UserConfig, item map[string]interface{}) {
+	for true {
+		fmt.Printf("Action[open(o), archive(a), delete(d), next(n), quit(q)] > ")
+		userAction, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		userAction = strings.Trim(strings.ToLower(userAction), " \n")
+
+		itemAction := func(action string) {
+			actions, err := json.Marshal([]map[string]string{
+				{"action": action, "item_id": item["item_id"].(string)},
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			requestData := url.Values{
+				"actions": {string(actions)},
+			}
+			requestPocketApi(cfg, "send", requestData)
+			fmt.Printf("Item #%s %sd :-)\n", item["item_id"], action)
+		}
+
+		switch userAction {
+		case "o", "open":
+			webbrowser.Open(item["resolved_url"].(string))
+		case "a", "archive":
+			itemAction("archive")
+			return
+		case "d", "delete":
+			itemAction("delete")
+			return
+		case "n", "next":
+			return
+		case "q", "quit":
+			os.Exit(0)
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func main() {
+	cfg := NewUserConfig().loadOrInitialize()
+	requestPermission(cfg)
+
 	// retrieve and go through items
-	// -----------------------------
 	if cfg.ApiKey != "" && cfg.UserCode != "" && cfg.UserToken != "" {
 		fmt.Printf("Hello %s!\n", cfg.Username)
 
-		request_url := fmt.Sprintf("%sget", API_BASE)
-		// request_data := map[string]interface{} {
-		request_data := url.Values{
-			"consumer_key": {cfg.ApiKey},
-			"access_token": {cfg.UserToken},
-			"detailType":   {"simple"},
-			"count":        {"10"},
-		}
-
-		fmt.Printf("Retrieving items from Pocket... ")
-
-		resp, err := http.PostForm(request_url, request_data)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		var data map[string]interface{}
-		err = json.Unmarshal(body, &data)
-		if err != nil {
-			log.Fatal(err)
-			os.Exit(1)
-		}
-
-		var items []map[string]interface{}
-		for _, item := range data["list"].(map[string]interface{}) {
-			items = append(items, item.(map[string]interface{}))
-		}
-
-		fmt.Printf("%d items retrieved!\n", len(items))
-
-		// shuffle by randomly swap items
-		for i := range items {
-			j := rand.Intn(len(items))
-			items[i], items[j] = items[j], items[i]
-		}
-
+		items := retrieveItems(cfg)
 		for i := range items {
 			item := items[i]
+			itemUnixTime, _ := strconv.Atoi(item["time_added"].(string))
+
 			fmt.Println("")
 			fmt.Printf("[#%s] \"%s\"\n", item["item_id"], item["resolved_title"])
 			fmt.Printf("%s\n", item["resolved_url"])
-			fmt.Printf("Added at %s\n", item["time_added"])
+			fmt.Printf("Added %s\n", prettyDateSince(itemUnixTime))
 
-			// user action
-		nextItem:
-			for true {
-				fmt.Printf("Action[open(o), archive(a), delete(d), next(n), quit(q)] > ")
-				consoleReader := bufio.NewReader(os.Stdin)
-				answer, err := consoleReader.ReadString('\n')
-				if err != nil {
-					log.Fatal(err)
-					os.Exit(1)
-				}
-				answer = strings.Trim(strings.ToLower(answer), " \n")
-
-				// FIXME: kinda dirty
-				itemAction := func(action string) {
-					request_url := fmt.Sprintf("%ssend", API_BASE)
-
-					actions, err := json.Marshal([]map[string]string{
-						{
-							"action":  action,
-							"item_id": item["item_id"].(string),
-						},
-					})
-					request_data := url.Values{
-						"consumer_key": {cfg.ApiKey},
-						"access_token": {cfg.UserToken},
-						"actions":      {string(actions)},
-					}
-
-					resp, err = http.PostForm(request_url, request_data)
-					if err != nil {
-						log.Fatal(err)
-						os.Exit(1)
-					}
-					defer resp.Body.Close()
-
-					if resp.StatusCode != 200 {
-						log.Fatal(
-							fmt.Sprintf("%s item error, StatusCode=[%d], X-Error-Code=[%s]",
-								action, resp.StatusCode, resp.Header.Get("X-Error-Code")))
-						os.Exit(1)
-					}
-					fmt.Printf("Item #%s %sd :-)\n", item["item_id"], action)
-				}
-
-				switch answer {
-				case "o", "open":
-					webbrowser.Open(item["resolved_url"].(string))
-				case "a", "archive":
-					itemAction("archive")
-					break nextItem
-				case "d", "delete":
-					itemAction("delete")
-					break nextItem
-				case "n", "next":
-					break nextItem
-				case "q", "quit":
-					os.Exit(0)
-				}
-			}
+			userInteractOnItem(cfg, item)
 		}
 	}
 }
